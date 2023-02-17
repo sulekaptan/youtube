737319556f34ba105eb5b8833188a34f52f5ff24
Şule <sule.kaptan@bil.omu.edu.tr>
2023-02-17 13:34:52 +0300

    add sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import SideBar from './SideBar'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <SideBar />
+      <Typography 
+        className='copyright'
+        variant='body2'
+        sx={{mt: 1.5, color: '#fff'}}
+      >
+        Tüm hakları saklıdır. 2023
+      </Typography>
+    </Box>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
+        New <span style={{color: '#F31503'}}>videos</span>
+      </Typography>
+    </Box>
+  </Stack>
   )
 }
 
