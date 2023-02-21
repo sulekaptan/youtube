d5a088755556ffb0b01befdbd94117b731273436
Şule <sule.kaptan@bil.omu.edu.tr>
2023-02-21 23:50:11 +0300

    add channeldetail

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,7 +25,7 @@ const Feed = () => {
         variant='body2'
         sx={{mt: 1.5, color: '#fff'}}
       >
-        Tüm hakları saklıdır. 2023
+        Copyright 2023
       </Typography>
     </Box>
     <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
